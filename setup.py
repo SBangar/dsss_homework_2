8b972b880510bb9e4e9227408373ef013bae2a20
SBangar <76514221+SBangar@users.noreply.github.com>
2023-11-13 22:57:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='math_quiz',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=[
-        'random'
-    ],
+    # install_requires=[
+    #     'random'
+    # ],
 )
