816f704d652df14ece5b9bcdef70ce3a20b2efe5
SBangar <76514221+SBangar@users.noreply.github.com>
2023-11-13 21:51:30 +0100

    Update math_quiz.py

--- math_quiz/math_quiz/math_quiz.py
+++ math_quiz/math_quiz/math_quiz.py
@@ -8,8 +8,7 @@ def minmax(min, max):
     - min (int): Minimum value of range given in parameter
     - max (int): Maximum value of range given in parameter
 
-    Returns:
-    int: A random integer between the minimum and maximum values
+    Returns a random integer between the minimum and maximum values
     """
     return random.randint(min, max)
 
@@ -25,17 +24,16 @@ def operation(num1, num2, op):
     """
     Function calculates the result of the specified arithmetic operation
 
-    Parameters:
+    Parameters -
     - num1 (int): First operand
     - num2 (int): Second operand.
     - op (str): Arithmetic operator ('+', '-', '*')
 
-    Returns:
-    tuple: A tuple containing the formatted arithmetic expression and the result
+    Returns a tuple containing the formatted arithmetic expression and the result
     """
     pholder = f"{num1} {op} {num2}"
-    if op == '+': ans = num1 - num2
-    elif op == '-': ans = num1 + num2
+    if op == '+': ans = num1 + num2
+    elif op == '-': ans = num1 - num2
     else: ans = num1 * num2
     return pholder, ans
 
@@ -55,14 +53,14 @@ def check_input():
 
 # Main function of the Math Quiz Game
 def math_quiz():
-    sum = 0
-    pvalue = 3
+    score = 0
+    totalq = 3
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    # Loop through a specified pvalue
-    for _ in range(pvalue):
+    # Loop through a specified totalq
+    for _ in range(totalq):
         n1 = minmax(1, 10); n2 = minmax(1, 5); o = randomsign()
 
         # Generate the problem and answer
@@ -73,12 +71,12 @@ def math_quiz():
         # Check user's answer and update the score
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            sum += -(-1)
+            score += -(-1)
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
     # Display final score to user
-    print(f"\nGame over! Your score is: {sum}/{pvalue}")
+    print(f"\nGame over! Your score is: {score}/{totalq}")
 
 if __name__ == "__main__":
     math_quiz()
